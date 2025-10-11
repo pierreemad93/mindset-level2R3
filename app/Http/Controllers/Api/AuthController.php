6c27bcb0dc2e1100ac64d9ed27832bd69a8c7a9e
pierreemad93 <pierreemad93@outlook.com>
2025-10-11 21:55:43 +0300

    Add Login and Logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\helpers\ApiResponse;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
 
@@ -32,5 +33,30 @@ class AuthController extends Controller
         $data['email'] = $user->email;
 
         return ApiResponse::response($data, 201, 'User Registerd Sucessfully');
+    }
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['required', 'string', Password::defaults()]
+        ]);
+        if ($validator->fails()) {
+            return  ApiResponse::response($validator->errors(), 422);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $logginUser = Auth::user();
+            $data['token'] = $logginUser->createToken('LogginUser')->plainTextToken;
+            $data['name'] = $logginUser->name;
+            $data['email'] = $logginUser->email;
+            return ApiResponse::response($data, 200);
+        } else {
+            return ApiResponse::response(null, 401, "User Doesn't be exsits");
+        }
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return ApiResponse::response("", 200, 'Logout Sucessfully');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,5 @@ Route::apiResource('users', UserController::class);
 
 //Auth
 Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
